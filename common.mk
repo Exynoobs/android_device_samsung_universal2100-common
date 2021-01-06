6fc2aea7c76c2d0909e560c1047aae5fdf5c0775
LuK1337 <priv.luk@gmail.com>
2021-01-06 19:30:19 +0100

    universal2100-common: Build vndservicemanager
    * Needed for FCM >= R.
    
    Change-Id: Ibc27a98f5774a51caaab077c1d8e0a2a79bdffe5

--- common.mk
+++ common.mk
@@ -243,6 +243,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.samsung
 
+# Vendor service manager
+PRODUCT_PACKAGES += \
+    vndservicemanager
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.samsung
