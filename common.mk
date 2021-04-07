034136cbcb342c877f66f77cc00ce0775e0236e3
Tim Zimmermann <tim@linux4.de>
2021-04-07 16:20:39 +0200

    universal2100-common: get access to all cameras
    * Possible camera IDs are from /vendor/etc/floating_feature.xml
    
    Change-Id: I5fb0fb57f5ea85c0c8ead5ddec2dba3cf0277f94

--- common.mk
+++ common.mk
@@ -75,7 +75,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.5-service_64 \
+    android.hardware.camera.provider@2.5-service_64.exynos2100 \
     libsensorndkbridge
 
 # Display
