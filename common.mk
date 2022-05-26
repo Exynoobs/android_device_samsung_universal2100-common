407255e3a27e671cfb18ca64c59d246e817ca994
Josip Kelecic <kelecicj@gmail.com>
2022-05-26 16:50:30 +0200

    universal2100-common: set shipping api level

--- common.mk
+++ common.mk
@@ -19,6 +19,8 @@ COMMON_PATH := device/samsung/universal2100-common
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/samsung/universal2100-common/universal2100-common-vendor.mk)
 
+PRODUCT_SHIPPING_API_LEVEL := 30
+BOARD_SHIPPING_API_LEVEL := 30
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_USE_DYNAMIC_PARTITION_SIZE := true
 PRODUCT_BUILD_SUPER_PARTITION := false
