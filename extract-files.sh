075ec61798f35814d51c8c304b166390a234c43f
Tim Zimmermann <tim@linux4.de>
2023-04-06 16:49:39 +0200

    universal2100-common: Replace libutils-v32 with a shim
    Change-Id: I792d3e1861f7ee721081997eeacc29c27aad3eef

--- extract-files.sh
+++ extract-files.sh
@@ -65,6 +65,7 @@ function blob_fixup() {
             ;;
         vendor/lib*/libexynosdisplay.so|vendor/lib*/hw/hwcomposer.exynos2100.so|vendor/lib*/sensors.*.so)
             "${PATCHELF}" --replace-needed libutils.so libutils-v32.so "${2}"
+            sed -i 's/_ZN7android6Thread3runEPKcim/_ZN7utils326Thread3runEPKcim/g' "${2}"
             ;;
         vendor/bin/hw/rild)
             "${PATCHELF}" --replace-needed libril.so libril-samsung.so "${2}"
